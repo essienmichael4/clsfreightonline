ea58cfc6fe78eb6351d2376695f4b1a12131703a
Code_Junior <essienmichael4@gmail.com>
2024-12-26 17:02:17 +0000

    Frontend login implementation of administrators

--- cslfreight_admin/src/App.tsx
+++ cslfreight_admin/src/App.tsx
@@ -4,16 +4,19 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import About from './pages/About'
+import RequireAuth from './components/RequireAuth'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/about' element={<About />} />
+        <Route path='/' element={<Home />} />
+        <Route element={<RequireAuth />}>
+          <Route element={<Layout />}>
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/about' element={<About />} />
+          </Route>
         </Route>
       </Routes>
     </>
--- cslfreight_admin/src/pages/Home.tsx
+++ cslfreight_admin/src/pages/Home.tsx
@@ -1,29 +1,30 @@
-import Faqs from "@/components/Faq"
-import Services from "../components/Services"
-import Hero from "../components/Hero"
-import Footer from "@/components/Footer"
-import Testimonials from "@/components/Testimonials"
+import shipping from "../assets/shipping.jpg"
+import logo from "../assets/logo.webp"
+import Login from "@/components/Login"
 
 const Home = () => {
   return (
     <>
-        {/* <Navbar /> */}
-        <div className="container mx-auto">
-        <div className='mx-auto px-6 max-w-7xl pt-12'>
-            <Hero />
-        </div>
-        </div>
-        <div className='mx-auto max-w-7xl '>
-            <Services />
-            <Testimonials />
-        </div>
-        <div className="container mx-auto">
-            <div className='mx-auto px-6 max-w-7xl '>
-                {/* <Process /> */}
-                <Faqs />
+        <div className="h-screen flex w-full max-h-[800px] items-center justify-between">
+            <div className="flex justify-center items-start w-full lg:w-[50%]">
+                <div className="w-3/4">
+                    <div className="w-16 h-16 mb-6">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <div>
+                        <h2 className="font-bold text-4xl mb-2">Sign in</h2>
+                        <p className="text-neutral-500 text-sm">Welcome to CCSL Freight Admin Panel - Let's help you sign in</p>
+                    </div>
+
+                    <Login />
+                </div>
+            </div>
+            <div className="w-full  lg:w-[50%] h-full p-4 rounded-lg overflow-hidden">
+                <div className={`w-full h-full bg-slate-500 flex bg-cover rounded-lg bg-center`} style={{backgroundImage:`url(${shipping})`}}>
+                    <div className="overlay w-full h-full bg-black opacity-40 sm:hidden"></div>
+                </div>
             </div>
         </div>
-        <Footer />
     </>
   )
 }
