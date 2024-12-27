9fe66112233b5d2b3b2300e9882e48969665ae73
Code_Junior <essienmichael4@gmail.com>
2024-12-27 14:17:13 +0000

    Fixed Footer corrections

--- cslfreight/src/pages/Contact.tsx
+++ cslfreight/src/pages/Contact.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
                 <div className="px-6  flex flex-wrap mt-10 rounded-lg justify-center items-center">
                     <div className="p-2 w-full lg:w-1/2 space-y-4">
                         <h2 className="text-3xl sm:text-5xl lg:text-6xl tracking-wide">
-                            We are here to help you with your vessel cleaning services
+                            We are here to help you with your freight forwarding services
                         </h2>
                     </div>
                     <div className="p-2 w-full lg:w-1/2">
@@ -25,28 +25,28 @@ const Contact = () => {
                 <div className="flex flex-wrap">
                     <div className="w-full md:w-1/2 lg:w-1/4 sm:px-4 py-2 mb-8">
                         <div className="border border-gray-300 p-4">
-                            <div className="bg-[#ff9d00]/50 w-10 h-10 flex items-center justify-center rounded-full">
-                                <MailCheck className="text-[#ff9d00] w-5 h-5"/>
+                            <div className="bg-blue-300/50 w-10 h-10 flex items-center justify-center rounded-full">
+                                <MailCheck className="text-blue-800 w-5 h-5"/>
                             </div>
-                            <p className="font-bold mt-12 text-lg">Send us message</p>
+                            <p className="font-bold mt-12 text-lg">Send us a message</p>
                             <p className="text-xs mt-3">Chat with our friendly team</p>
-                            <p className="test-sm mt-8">kojosavage@ymail.com</p>
+                            <p className="test-sm mt-8">contact@rmbdeals.com</p>
                         </div>
                     </div>
                     <div className="w-full md:w-1/2 lg:w-1/4 sm:px-4 py-2 mb-8">
                         <div className="border border-gray-300 p-4">
-                            <div className="bg-[#ff9d00]/50 w-10 h-10 flex items-center justify-center rounded-full">
-                                <PhoneCall className="text-[#ff9d00] w-5 h-5"/>
+                            <div className="bg-blue-300/50 w-10 h-10 flex items-center justify-center rounded-full">
+                                <PhoneCall className="text-blue-800 w-5 h-5"/>
                             </div>
                             <p className="font-bold mt-12 text-lg">Call us</p>
-                            <p className="text-xs mt-3">Mon-Sun, anytime.</p>
-                            <p className="test-sm mt-8">(+233) 24 366 0662</p>
+                            <p className="text-xs mt-3">Mon-Sat, 8:00am - 8:00pm (GMT).</p>
+                            <p className="test-sm mt-8">(+233) 244 699 112</p>
                         </div>
                     </div>
                     <div className="w-full lg:w-2/4 sm:px-4 py-2 mb-8">
                         <div className="border border-gray-300 p-4">
-                            <div className="bg-[#ff9d00]/50 w-10 h-10 flex items-center justify-center rounded-full">
-                                <MapPinned className="text-[#ff9d00] w-5 h-5"/>
+                            <div className="bg-blue-300/50 w-10 h-10 flex items-center justify-center rounded-full">
+                                <MapPinned className="text-blue-800 w-5 h-5"/>
                             </div>
                             <p className="font-bold mt-2 text-lg">Vists our office</p>
                         </div>
@@ -57,7 +57,7 @@ const Contact = () => {
                 <h2 className="text-3xl sm:text-5xl lg:text-6xl tracking-wide mb-12">
                     Meet the management team
                 </h2>
-                <div className="flex flex-wrap">
+                {/* <div className="flex flex-wrap">
                     <div className="w-full md:w-1/2 lg:w-1/4 sm:px-4 py-2 mb-8">
                         <div className="w-full h-96 bg-black rounded-xl">
                             
@@ -76,7 +76,7 @@ const Contact = () => {
                             <p className="text-lg mt-2">Chief Operating Officer</p>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
         <Footer />
