8f527fcd93cbbe8eb86e2c72e47a83ad6924b418
Code_Junior <essienmichael4@gmail.com>
2024-12-30 15:11:09 +0000

    create package implementation

--- csl_be_ty/src/package/package.controller.ts
+++ csl_be_ty/src/package/package.controller.ts
@@ -1,32 +1,60 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, ParseIntPipe } from '@nestjs/common';
 import { PackageService } from './package.service';
 import { CreatePackageDto } from './dto/create-package.dto';
 import { UpdatePackageDto } from './dto/update-package.dto';
+import { PackageRequest } from './dto/package.dto';
+import { JwtGuard } from 'src/guards/jwt.guard';
+import { User, UserInfo } from 'src/decorators/user.decorator';
 
-@Controller('package')
+@Controller('packages')
 export class PackageController {
   constructor(private readonly packageService: PackageService) {}
 
+  @UseGuards(JwtGuard)
   @Post()
-  create(@Body() createPackageDto: CreatePackageDto) {
-    return this.packageService.create(createPackageDto);
+  create(@Body() body: PackageRequest, @User() user:UserInfo) {
+    return this.packageService.create({
+      customer: body.customer, 
+      trackingNumber: body.trackingNumber,
+      cbm: body.cbm,
+      quantity: body.quantity,
+      package: body.package,
+      email: body.email,
+      phone: body.phone,
+      vessel: body.vessel,
+      status: body.status,
+      addedBy: user.sub.id,
+      loaded: body.loaded,
+      eta: body.eta,
+      received: body.received
+    });
   }
 
+  @UseGuards(JwtGuard)
   @Get()
   findAll() {
     return this.packageService.findAll();
   }
 
+  @UseGuards(JwtGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.packageService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.packageService.findOneById(+id);
   }
 
+  @UseGuards(JwtGuard)
+  @Get(':trackingNumber')
+  findWithTrackingNumber(@Param('trackingNumber', ParseIntPipe) trackingNumber: string) {
+    return this.packageService.findOneByTrackingNumber(trackingNumber);
+  }
+
+  @UseGuards(JwtGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePackageDto: UpdatePackageDto) {
     return this.packageService.update(+id, updatePackageDto);
   }
 
+  @UseGuards(JwtGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.packageService.remove(+id);
--- csl_be_ty/src/package/package.service.ts
+++ csl_be_ty/src/package/package.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePackageDto } from './dto/create-package.dto';
 import { UpdatePackageDto } from './dto/update-package.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Package } from './entities/package.entity';
+import { Repository } from 'typeorm';
+import { PackageEdit } from './entities/packageEdits.entity';
 
 @Injectable()
 export class PackageService {
-  create(createPackageDto: CreatePackageDto) {
-    return 'This action adds a new package';
+  constructor(
+    @InjectRepository(Package) private readonly packageRepo:Repository<Package>, 
+    @InjectRepository(PackageEdit) private readonly packageEditRepo:Repository<PackageEdit>
+  ){}
+
+  async create(createPackageDto: CreatePackageDto) {
+    const packaged = this.packageRepo.create(createPackageDto)
+    return await this.packageRepo.save(packaged);
   }
 
-  findAll() {
-    return `This action returns all package`;
+  async findAll() {
+    return await this.packageRepo.find({
+      order: {
+        id: "DESC", 
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} package`;
+  async findOneById(id: number) {
+    return await this.packageRepo.findOneBy({id});
+  }
+
+  async findOneByTrackingNumber(trackingNumber: string) {
+    return await this.packageRepo.findOneBy({trackingNumber});
   }
 
   update(id: number, updatePackageDto: UpdatePackageDto) {
